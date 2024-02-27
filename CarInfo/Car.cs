66b0133653d8779c842c68864337ca341af8cf99
ELMURODSALOMOV <salomovelmurod062@gmail.com>
2024-02-27 17:48:18 +0500

    About Cars

---
+++ CarInfo/Car.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cars.CarInfo
+{
+    public class Car
+    {
+        public string name;
+        public string color;
+        public int position;
+        public int number;
+    
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+using Cars.CarInfo;
+
+Car BMW = new Car();
+BMW.name = "BMW";
+BMW.color = "Red";
+BMW.position = 1;
+BMW.number = 111;
+
+Car Mers = new Car();
+Mers.name = "Mers";
+Mers.color = "Black";
+Mers.position = 2;
+Mers.number = 222;
+
+Car Volvo = new Car();
+Volvo.name = "Volvo";
+Volvo.color = "Green";
+Volvo.position = 3;
+Volvo.number = 333;
+
+Car Ford = new Car();
+Ford.name = "Ford";
+Ford.color = "Yellow";
+Ford.position = 4;
+Ford.number = 444;
